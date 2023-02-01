8660f32f33dd1084cd306b861c9cfb9bd0a57e27
gautte <s362066@oslomet.no>
2023-02-01 11:56:05 +0100

    Completed task 1 and 2

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # Write a function named jains that takes two throughput values (int and/or float) as arguments and
 # returns a JFI
 
-
+"""
 def jains():
     throughput1 = float(input("Enter throughput1: "))
     throughput2 = float(input("Enter throughput2: "))
@@ -14,14 +14,23 @@ def jains():
 
 print("The Jains Fairness Index is: ", jains())
 
-
+"""
 # Task 2
 # Write a new function jainsall function that takes a list of throughput values (integers and/or float)
 # and returns a JFI
 
 throughputsAmount = int(input("Amount of throughputs: "))
 
+throughputs = []
 for num in range(throughputsAmount):
-    float(input("Value of throughput: "))
+    throughput = float(input("Value of throughput: "))
+    throughputs.append(throughput)
 
 
+def jainsall(throughputs):
+    jfi = (sum([x ** 2 for x in throughputs])) / (throughputsAmount * sum(throughputs))
+    jfi = round(jfi, 4)
+    return jfi
+
+
+print(jainsall(throughputs))
